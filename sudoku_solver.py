2d20663650930e28f9d377df66935b284d80b89c
ChristopheVdE <46569386+ChristopheVdE@users.noreply.github.com>
2019-11-02 18:20:08 +0100

    input

---
+++ sudoku_solver.py
@@ -0,0 +1,24 @@
+# SUDOKU SOLVER
+
+# INPUT THE BOARD ======================================================================================
+board = []
+for i in range(1, 10):
+    line = input("Line {}: ".format(i))
+    # CHECK IF INPUT MEETS REQUIREMENTS-----------------------------------------------------------------
+    while not len(line) == 9:
+        if len(line) <= 9:
+            print("[ERROR] Line is to short, a line must contain 9 numbers")
+            if not line.isdigit():
+                print("[ERROR] Line can only contain numbers")
+        else:
+            print("[ERROR] Line is to long, a line must contain 9 numbers")
+            if not line.isdigit():
+                print("[ERROR] Line can only contain numbers")
+        line = input("Line {}: ".format(i))
+    # SAVE INPUT INTO BOARD ----------------------------------------------------------------------------
+    row = []
+    for num in line:
+        row.append(num)
+    board.append(row)
+print(board)
+# ======================================================================================================
