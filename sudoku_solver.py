ba19eac2ea97e2696baa49b73d41875ad7c0b284
ChristopheVdE <46569386+ChristopheVdE@users.noreply.github.com>
2019-11-02 18:25:47 +0100

    better error catching for input

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -5,15 +5,13 @@ board = []
 for i in range(1, 10):
     line = input("Line {}: ".format(i))
     # CHECK IF INPUT MEETS REQUIREMENTS-----------------------------------------------------------------
-    while not len(line) == 9:
-        if len(line) <= 9:
+    while not len(line) == 9 or not line.isdigit():
+        if len(line) < 9:
             print("[ERROR] Line is to short, a line must contain 9 numbers")
-            if not line.isdigit():
-                print("[ERROR] Line can only contain numbers")
-        else:
+        if len(line) > 9:
             print("[ERROR] Line is to long, a line must contain 9 numbers")
-            if not line.isdigit():
-                print("[ERROR] Line can only contain numbers")
+        if not line.isdigit():
+            print("[ERROR] Line can only contain numbers")
         line = input("Line {}: ".format(i))
     # SAVE INPUT INTO BOARD ----------------------------------------------------------------------------
     row = []
