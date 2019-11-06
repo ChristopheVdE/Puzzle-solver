514f69a3172dd15dbcfca8a615f2bb72a99f3b96
ChristopheVdE <46569386+ChristopheVdE@users.noreply.github.com>
2019-11-06 15:18:20 +0100

    sudoku solver done

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -20,16 +20,29 @@
 #     board.append(row)
 # # ======================================================================================================
 
+# board = [
+#     [7, 8, 0, 4, 0, 0, 1, 2, 0],
+#     [6, 0, 0, 0, 7, 5, 0, 0, 9],
+#     [0, 0, 0, 6, 0, 1, 0, 7, 8],
+#     [0, 0, 7, 0, 4, 0, 2, 6, 0],
+#     [0, 0, 1, 0, 5, 0, 9, 3, 0],
+#     [9, 0, 4, 0, 6, 0, 0, 0, 5],
+#     [0, 7, 0, 3, 0, 0, 0, 1, 2],
+#     [1, 2, 0, 0, 0, 7, 4, 0, 0],
+#     [0, 4, 9, 2, 0, 6, 0, 0, 7],
+# ]
+
+# Hardest sudoku ever
 board = [
-    [7, 8, 0, 4, 0, 0, 1, 2, 0],
-    [6, 0, 0, 0, 7, 5, 0, 0, 9],
-    [0, 0, 0, 6, 0, 1, 0, 7, 8],
-    [0, 0, 7, 0, 4, 0, 2, 6, 0],
-    [0, 0, 1, 0, 5, 0, 9, 3, 0],
-    [9, 0, 4, 0, 6, 0, 0, 0, 5],
-    [0, 7, 0, 3, 0, 0, 0, 1, 2],
-    [1, 2, 0, 0, 0, 7, 4, 0, 0],
-    [0, 4, 9, 2, 0, 6, 0, 0, 7],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 3, 6, 0, 0, 0, 0, 0],
+    [0, 7, 0, 0, 9, 0, 2, 0, 0],
+    [0, 5, 0, 0, 0, 7, 0, 0, 0],
+    [0, 0, 0, 0, 4, 5, 7, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 3, 0],
+    [0, 0, 1, 0, 0, 0, 0, 6, 8],
+    [0, 0, 8, 5, 0, 0, 0, 1, 0],
+    [0, 9, 0, 0, 0, 0, 4, 0, 0],
 ]
 
 # FUNCTIONS ============================================================================================
@@ -80,38 +93,75 @@ def box(board, empty_pos):
 
 
 # TEST FOR CERTAIN VALUES ------------------------------------------------------------------------------
-def certain(board):
-    for i in find_empty(board):  # i = tuple(row, col)
-        test = []
-        print("row = {}".format(board[i[0]]))
-        print("col = {}".format(column(board, i[1])))
-        print("box = {}".format(box(board, i)))
-        # check values
-        for value in range(1, 10):
-            # search row and column
-            if (
-                not value in board[i[0]]  # rox
-                and not value in column(board, i[1])  # column
-                and not value in box(board, i)  # box
-            ):
-                test.append((value, True))
-            else:
-                test.append((value, False))
-        print(i, test)
+def certain(board, empty_pos):
+    test = []
+    # check for possible values (1-9)
+    for value in range(1, 10):
+        # search row, column and box
+        if (
+            not value in board[empty_pos[0]]  # rox
+            and not value in column(board, empty_pos[1])  # column
+            and not value in box(board, empty_pos)  # box
+        ):
+            test.append((value, True))
     return test
+
+
+# BRUTE FORCE (RECURSIVE) ------------------------------------------------------------------------------
+def brute_force(board):
+    # FIND EMPTY POSITIONS ON BOARD --------------------------------------------------------------------
+    empty_pos = find_empty(board)
+    if not empty_pos:
+        # solution found
+        return True
+    else:
+        empty_pos = empty_pos[0]
+
+    # CHECK POSSIBLE VALUES FOR EMPTY POSITION & UPDATE BOARD IF FOUND ---------------------------------
+    for value in range(1, 10):
+        # search row, column and box
+        if (
+            not value in board[empty_pos[0]]  # rox
+            and not value in column(board, empty_pos[1])  # column
+            and not value in box(board, empty_pos)  # box
+        ):
+            # update board if value is valid
+            board[empty_pos[0]][empty_pos[1]] = value
+            # try a value in the next empty position if a valid value was inserted
+            if brute_force(board):
+                return True
+            board[empty_pos[0]][empty_pos[1]] = 0
+    return False
 
 
 # ======================================================================================================
 
 
 # SOLVE ================================================================================================
-# while len(find_empty(board)) != 0:
-#     for i in find_empty(board):
-# check if a value can be inserted with 100% certainty at this position
-# insert value at position
-
+# PRINT ORIGINAL BOARD ---------------------------------------------------------------------------------
 print_sudoku(board)
-print("empty values: {}".format(find_empty(board)))
-# print("box_values: {}".format(box(board, (3, 3))))
-print(certain(board))
+
+# CHECK FOR CERTAIN VALUES & UPDATE BOARD IF FOUND -----------------------------------------------------
+while len(find_empty(board)) != 0:
+    og_board = board
+    # SEARCH FOR CERTAIN VALUES AND UPDATE BOARD IF FOUND ----------------------------------------------
+    for empty_pos in find_empty(board):
+        # print("row = {}".format(board[empty_pos[0]]))
+        # print("col = {}".format(column(board, empty_pos[1])))
+        # print("box = {}".format(box(board, empty_pos)))
+        # print(empty_pos, certain(board, empty_pos))
+        if len(certain(board, empty_pos)) == 1:
+            board[empty_pos[0]][empty_pos[1]] = certain(board, empty_pos)[0][0]
+    # BREAK LOOP IF NO MORE CERTAIN VALUES (NO BOARD UPDATES) ------------------------------------------
+    if og_board == board:
+        break
+
+# USE BRUTE FORCE IF THERE ARE STILL EMPTY VALUES AND THERE ARE NO MORE CERTAIN VALUES -----------------
+if (len(find_empty(board)) != 0) and (og_board == board):
+    brute_force(board)
+
+# PRINT SOLVED BOARD -----------------------------------------------------------------------------------
+print()
+print_sudoku(board)
+
 # ======================================================================================================
