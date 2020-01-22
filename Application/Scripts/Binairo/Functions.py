50801f53caa2ca4f8e25c891b7ae7c8913410918
ChristopheVdE <46569386+ChristopheVdE@users.noreply.github.com>
2020-01-22 16:07:58 +0100

    better controle over selected cube
    as soon as you move the mouse away from teh selcted cube, it gets de-selected

--- Application/Scripts/Binairo/Functions.py
+++ Application/Scripts/Binairo/Functions.py
@@ -125,6 +125,10 @@ class board():
                             BoardPos = (row, col)
                             CubeCoords = (self.Cols[col][0], self.Rows[row][1])
                             self.selected = ("R", BoardPos, CubeCoords)
+        if self.selected:
+            if not self.selected[2][0] + self.CubeSize > mouse[0] > self.selected[2][0] and not self.selected[2][1] + self.CubeSize > mouse[0] > self.selected[2][1]:
+                self.selected = None
+
 # Pencil values for the selected cube (temp values)---------------------------------------------------------
     def Pencil(self, key=None):
         # Pencil in values ---------------------------------------------------------------------------------
