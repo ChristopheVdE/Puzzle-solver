d1af3bcb119ccc30852a58f34e38edecdd90a725
ChristopheVdE <46569386+ChristopheVdE@users.noreply.github.com>
2021-09-05 11:41:03 +0200

    Update CheckAgaintSolution.py

--- Application/Scripts/Test/Functions/CheckAgaintSolution.py
+++ Application/Scripts/Test/Functions/CheckAgaintSolution.py
@@ -1,12 +1,13 @@
 ############################################################################################################
-# Name: CheckBoard.py
-# Function: check the board against the solution, remove all wrong values and set al correct values al correct
+# Name: CheckAgainstSolution.py
+# Function: check the board against the solution, remove all wrong values and set al correct values as correct
 ############################################################################################################
 
-def CheckBoard(pCurrentBoard, pSolution):
+def CheckAgainstSolution(pCurrentBoard, pSolution):
     for RowNr in range(pCurrentBoard.NumberOfRows):
         for ColNr in range(pCurrentBoard.NumberOfColumns):
-            if pCurrentBoard.getRow(RowNr)[ColNr].GetValue() == pSolution.GetRow(RowNr)[ColNr].GetValue():
-                pCurrentBoard.getRow(RowNr)[ColNr].SetValueAsCorrect()
+            if pCurrentBoard.GetRow(RowNr)[ColNr].GetValue() == pSolution.GetRow(RowNr)[ColNr].GetValue():
+                pCurrentBoard.GetRow(RowNr)[ColNr].SetValueAsCorrect()
             else:
-                pCurrentBoard.getRow(RowNr)[ColNr].UpdateValue(0)
+                pCurrentBoard.GetRow(RowNr)[ColNr].UpdateValue(0, pCurrentBoard, (RowNr, ColNr))
+    return pCurrentBoard
